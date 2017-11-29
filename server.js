02945c239df06a04d653858b277342b83bdc78ff
Keith Wolf <keith.c.wolf@abc.com>
2017-11-28 16:01:35 -0800

    made it work

--- server.js
+++ server.js
@@ -8,8 +8,86 @@ app.use(express.static(__dirname + '/static'))
 
 app.get('/', function (req, res, next) {
   try {
+    var url = require("url");
+    var page = url.parse(req.url).pathname;
+    console.log(page);
+
     var html = template({ title: 'Home' })
     res.send(html)
+  } catch (e) {
+    next(e)
+  }
+})
+
+app.get('/test', function (req, res, next) {
+  try {
+    var querystring = require('querystring');
+    var url = require("url");
+    var page = url.parse(req.url).pathname;
+    var query = url.parse(req.url).query;
+    var params = querystring.parse(query);
+    console.log(page);
+    //var html = template({ title: 'Data' })
+    res.send("<html><body><h1>Hello World!</h1>" + params['firstName'] + "</body></html>")
+  } catch (e) {
+    next(e)
+  }
+})
+
+app.get('/data', function (req, res, next) {
+  try {
+
+    // config for your database
+    var sql = require("mssql");
+    sql.close();
+    
+    // config for your database
+    var config = {
+      user: 'callcenter',
+      password: 'callcenter',
+      server: '10.102.168.22', 
+      database: 'KCW',
+      port: 1433
+    };
+
+    console.log("SQL Connecting");
+
+    // connect to your database
+    sql.connect(config, function (err) {
+      
+
+      if (err) console.log(err);
+
+      console.log("SQL Connected");
+      
+      // create Request object
+      var request = new sql.Request();
+          
+      // query to the database and get the records
+      request.query('select TOP 20 * from Ticket_Coding_Mapping', function (err, recordset) {
+          
+          if (err) console.log(err)
+
+          // send records as a response
+          //res.send(recordset);
+
+          var html = template({ title: 'Home' })
+
+          res.writeHead(200, { 'Content-Type': 'text/html' });
+          res.write("<html><table>");
+          for (var i = 0; i < recordset.length; i++) {
+            res.write("<tr><td>" + recordset[i].DisplayName + "</td><td>" + recordset[i].ApplicationID + "</td></tr>" );
+          }
+
+          res.write("</table></html>");
+          res.end("<br />Done.");
+      });
+    });
+
+    console.log("Done");
+    
+    //sql.close();
+
   } catch (e) {
     next(e)
   }
